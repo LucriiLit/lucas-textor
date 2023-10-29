df6d695928499126cce0c7fac3e833b6affa3c32
LucriiLit <93228893+LucriiLit@users.noreply.github.com>
2023-10-29 16:16:24 +0100

    replaced sketch w p5

--- src/p5_algorithms/P5Canvas02.js
+++ src/p5_algorithms/P5Canvas02.js
@@ -12,100 +12,94 @@ function P5Canvas02() {
     if (isInitialized) return;
 
     canvasInst.current = new p5(
-      (sketch) => {
+      (p5) => {
         let firstRun = true;
         let runCounter = 0;
         let r = 50;
 
-        sketch.setup = function () {
+        p5.setup = function () {
           const container = canvasRef02.current.parentElement;
           const containerSize = container.getBoundingClientRect();
-          const canvs = sketch.createCanvas(
+          const canvs = p5.createCanvas(
             containerSize.width,
             containerSize.height
           );
-          canvs.mouseOver(() => sketch.loop());
-          canvs.mouseOut(() => sketch.noLoop());
+          canvs.mouseOver(() => p5.loop());
+          canvs.mouseOut(() => p5.noLoop());
           setIsInitialized(true);
-          sketch.blendMode(sketch.BLEND);
-          sketch.colorMode(sketch.HSL);
-          sketch.background(50, 20, 10);
+          p5.blendMode(p5.BLEND);
+          p5.colorMode(p5.HSL);
+          p5.background(50, 20, 10);
         };
 
-        sketch.draw = function () {
+        p5.draw = function () {
           const container = canvasRef02.current.parentElement;
           const containerSize = container.getBoundingClientRect();
 
-          sketch.blendMode(sketch.BLEND);
-          sketch.background(50, 20, 0, 0.02);
-          sketch.translate(containerSize.width / 2, containerSize.height / 2);
-          sketch.noFill();
-          sketch.strokeWeight(sketch.random(0, 1));
-          sketch.stroke(sketch.random(10, 70), 100, sketch.random(50, 80));
+          p5.blendMode(p5.BLEND);
+          p5.background(50, 20, 0, 0.02);
+          p5.translate(containerSize.width / 2, containerSize.height / 2);
+          p5.noFill();
+          p5.strokeWeight(p5.random(0, 1));
+          p5.stroke(p5.random(10, 70), 100, p5.random(50, 80));
 
-          let increment = sketch.map(
-            sketch.mouseX,
+          let increment = p5.map(
+            p5.mouseX,
             100,
             containerSize.width,
             0.01,
-            sketch.PI
+            p5.PI
           );
 
-          let circleWidth = sketch.map(
-            sketch.mouseX,
-            100,
-            containerSize.width,
-            0,
-            10
-          );
+          let circleWidth = p5.map(p5.mouseX, 100, containerSize.width, 0, 10);
 
           if (circleWidth > 5) {
-            sketch.stroke(sketch.random(180, 270), 100, sketch.random(50, 80));
+            p5.stroke(p5.random(180, 270), 100, p5.random(50, 80));
           } else if (circleWidth < 3) {
-            sketch.stroke(360, 100, 100);
+            p5.stroke(360, 100, 100);
           }
 
           let circleCounter = 0.05;
 
-          sketch.beginShape();
-          for (let a = 0; a < sketch.TWO_PI; a += circleCounter) {
-            let r1 = r + sketch.random(-20, 20);
-            let x = r1 * sketch.cos(a);
-            let y = r1 * sketch.sin(a);
+          p5.beginShape();
+          for (let a = 0; a < p5.TWO_PI; a += circleCounter) {
+            let r1 = r + p5.random(-20, 20);
+            let x = r1 * p5.cos(a);
+            let y = r1 * p5.sin(a);
 
             if (circleWidth < 1) {
               circleCounter = 0.2;
             }
 
-            // circleCounter = sketch.map(
-            //   sketch.mouseX,
+            // circleCounter = p5.map(
+            //   p5.mouseX,
             //   100,
             //   containerSize.width,
             //   0.1,
             //   0.02
             // );
 
-            sketch.circle(x, y, sketch.random(0, circleWidth));
+            p5.circle(x, y, p5.random(0, circleWidth));
 
             r = increment * 100;
-            sketch.print(containerSize.width);
+            p5.print(containerSize.width);
           }
 
-          sketch.endShape(sketch.CLOSE);
+          p5.endShape(p5.CLOSE);
 
           if (runCounter < 20) {
             runCounter += 1;
             if (firstRun && runCounter === 20) {
               firstRun = false;
-              sketch.noLoop();
+              p5.noLoop();
             }
           }
         };
 
-        sketch.windowResized = function () {
+        p5.windowResized = function () {
           const container = canvasRef02.current.parentElement;
           const containerSize = container.getBoundingClientRect();
-          sketch.resizeCanvas(containerSize.width, containerSize.height);
+          p5.resizeCanvas(containerSize.width, containerSize.height);
         };
       },
       canvasRef02.current,
