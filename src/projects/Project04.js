9def505c5eb6b09302f2b4a3a98eac01b7f00e0b
LucriiLit <luc.textor@gmail.com>
2025-01-10 15:00:59 +0100

    January Brain Toast added

--- src/projects/Project04.js
+++ src/projects/Project04.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { motion } from "framer-motion";
-import teaserKardios from "../images/webDev_Kardios-Mockup-02.png";
-import teaserLCT from "../images/webDev_LCT-Mockup-02.png";
-import teaserWVM from "../images/webDev_Wirtschaft-von-Morgen-Mockup-02.png";
+import teaserKardios from "../images/webDev_Kardios-Mockup-03.png";
+import teaserLCT from "../images/webDev_LCT-Mockup-03.png";
+import teaserWVM from "../images/webDev_Wirtschaft-von-Morgen-Mockup-03.png";
+import teaserBT from "../images/webDev_Brain-Toast-MockUp-03.png";
 
 
 function Project04() {
@@ -29,13 +30,45 @@ function Project04() {
       <h1>Transferring Visual Concepts to the Web</h1>
       <br></br>
       <p>
-        <img src={teaserKardios} alt="Screendesign of Kardios Website" className="borderRadius" />
-        [2021] &#47;&#47; Website for two medical practices of cardiologists <br></br>
-        <a href="https://www.kardios.de" target="_blank" rel="noreferrer">
+        <img src={teaserBT} alt="Screendesign of Kardios Website" className="borderRadius" />
+        [2024] &#47;&#47; Website for a management consultant for knowledge and technology transfer <br></br>
+        <a href="https://www.braintoast.de" target="_blank" rel="noreferrer">
           <br></br>
-          &#x21B3; <u>kardios.de</u>
+          &#x21B3; <u>braintoast.de</u>
         </a>
       </p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>
+        <img src={teaserLCT} alt="Screendesign of Kardios Website" className="borderRadius" />
+        [2023] &#47;&#47; Coaching funnels for a business psychologist <br></br>
+        <a
+          href="https://www.light-creators.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <br></br>
+          &#x21B3; <u>light-creators.com</u>
+        </a>
+        <a
+          href="https://www.light-creators.com/buch"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <br></br>
+          &#x21B3; <u>/buch</u>
+        </a>
+        <a
+          href="https://www.light-creators.com/video-training"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <br></br>
+          &#x21B3; <u>/video-training</u>
+        </a>
+      </p>      
       <br></br>
       <br></br>
       <br></br>
@@ -57,23 +90,11 @@ function Project04() {
       <br></br>
       <br></br>
       <p>
-        <img src={teaserLCT} alt="Screendesign of Kardios Website" className="borderRadius" />
-        [2023] &#47;&#47; Coaching funnels for a business psychologist <br></br>
-        <a
-          href="https://www.light-creators.com"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <img src={teaserKardios} alt="Screendesign of Kardios Website" className="borderRadius" />
+        [2021] &#47;&#47; Website for two medical practices of cardiologists <br></br>
+        <a href="https://www.kardios.de" target="_blank" rel="noreferrer">
           <br></br>
-          &#x21B3; <u>light-creators.com</u>
-          <a
-            href="https://www.light-creators.com/video-training"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <br></br>
-            &#x21B3; <u>/video-training</u>
-          </a>
+          &#x21B3; <u>kardios.de</u>
         </a>
       </p>
       <br></br>
